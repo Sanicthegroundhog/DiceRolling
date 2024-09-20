e6012bd1eb3c3d0e6808f09252198a0ec4373f41
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-09-20 13:32:25 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) {
+
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+    }
+
+    private static void rollSixDice() {
+        Random rand = new Random();
+        System.out.println("6 dices roll: ");
+        for(int i = 0; i < 6; i++) {
+            int roll = rand.nextInt(6) + 1;
+            System.out.print("Roll #" + (i + 1) + ": " + roll + " ");
+        }
+        System.out.println();
+    }
+}
